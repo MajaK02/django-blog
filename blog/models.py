f03e469f03e79dbe13b0eb89dcc1198659492b83
Maja Kazmierczak <upgbhvet@students.codeinstitute.net>
2025-01-06 17:05:13 +0000

    Add comment model and migrate it to migrations & add to admin.py

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,15 @@ class Post(models.Model):
     content = models.TextField() # blog article content 
     created_on = models.DateTimeField(auto_now_add=True) # date and time the article was created/ The auto_now_add=True means the default created time is the time of post entry.
     status = models.IntegerField(choices=STATUS, default=0) # status of the article. 0 is draft, 1 is published.
+    excerpt = models.TextField(blank=True) # a short description of the article.
+    updated_on = models.DateTimeField(auto_now=True) # date and time the article was last updated.
+
+
+# Comment Model
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
